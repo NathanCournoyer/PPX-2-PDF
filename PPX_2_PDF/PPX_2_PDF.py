cd97bfd7c6c5c11301bf2400dd9530d38dec9a06
NathanCournoyer <cournoyernat@gmail.com>
2024-10-16 13:56:52 -0400

    while loop to validate file path if not found

--- PPX_2_PDF/PPX_2_PDF.py
+++ PPX_2_PDF/PPX_2_PDF.py
@@ -13,20 +13,19 @@ def main() -> int:
     # CONST
     DEFAULT_PDF_DIR: final = "G:\Mon disque\shared\\2024\AUT\COM120\PDF"
     DEFAULT_PPX_DIR: final = "G:\Mon disque\shared\\2024\AUT\COM120\PPX"
+    PPX_2_PDF = sys.argv[0]
 
     # Variables
     pdf_str_dir: str = ""
     ppx_str_dir: str = ""
-    exit_code = 0
-    
-    # Defining script arguments;
-    PPX_2_PDF = sys.argv[0]
+    exit_code: int = 0
+    valid_file_path: bool = False    
 
     # No real arg handling for now, one teacher sole reason this exists, const dir
     print(f"Script {PPX_2_PDF} running: {sys.argv}")
     
     if len(sys.argv) > 1:
-        # The PowerPoint folder provided.
+        # The PowerPoint folder provided as 2nd arg.
         print('Found PowerPoint dir arg.')
         ppx_str_dir = sys.argv[1]
     else : 
@@ -34,17 +33,29 @@ def main() -> int:
         ppx_str_dir= DEFAULT_PPX_DIR
 
     if len(sys.argv) > 2:
-        # The PDF folder provided.
+        # The PDF folder provided as 3rd arg.
         print('Found PDF dir arg.')
         pdf_str_dir = sys.argv[2]
     else:
         # Default PDF location
         pdf_str_dir = DEFAULT_PDF_DIR
 
+
     
     # Iterables
-    ppx_bytes_dir: bytes  = os.fsencode(ppx_str_dir) 
-    pdf_bytes_dir: bytes = os.fsencode(pdf_str_dir)
+    while (not valid_file_path):
+        try :
+            ppx_bytes_dir: bytes  = os.fsencode(ppx_str_dir) 
+            pdf_bytes_dir: bytes = os.fsencode(pdf_str_dir)
+
+            # Valid File Path Loop Exit
+            valid_file_path = True
+        except FileNotFoundError as file_not_found_error:
+            print(f"File not found: {file_not_found_error}")
+            ppx_bytes_dir = input("Enter PowerPoint source Directory: ")
+            pdf_bytes_dir = input("Enter PDF target Directory: ")
+
+
 
     # https://stackoverflow.com/a/10378012 CC BY-SA 4.0
     for file in os.listdir(ppx_bytes_dir):
@@ -55,7 +66,7 @@ def main() -> int:
             try: 
                 print(f"{filename}.pdf will be placed in {output_filename}")
                 PPTtoPDF(input_filename, output_filename)
-            except FileNotFoundError as file_error:
+            except Exception as file_error:
                 print(f"Oopsie daisies: {file_error}. It seems like the path is wrong or inaccessible.")
                 exit_code = -1
         else:
@@ -64,15 +75,15 @@ def main() -> int:
     print(f"Script {PPX_2_PDF} exiting: {exit_code}")
     return exit_code
 
-# Sequential unsorted search pdf dir, check if the ppx is converted already
+# Sequential search through pdf dir, check if the ppx is converted already
 def is_ppx_file_in_pdf_dir(ppx_filename: str, pdf_bytes_dir: bytes) -> bool:
-    file_is_present = False
+    # Iterable list[bytes] representing PDF files.
     pdf_files = os.listdir(pdf_bytes_dir)
     for file in pdf_files:
         filename = os.fsdecode(file)
         if filename == ppx_filename + ".pdf":
             return True
-    return file_is_present
+    return False
 
 # https://stackoverflow.com/a/31624001 CC BY-SA 4.0
 def PPTtoPDF(inputFileName, outputFileName):
